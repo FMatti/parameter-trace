4cf9b6bc63d7948a3a7d922d442492ac7cb94088
FMatti <fabio.matti@epfl.ch>
2024-06-27 16:59:34 +0200

    make proof-read adjustments

--- paper/plots/zerocheck.py
+++ paper/plots/zerocheck.py
@@ -1,36 +1,3 @@
-#import __context__
-#
-#import numpy as np
-#import scipy as sp
-#import matplotlib.pyplot as plt
-#
-#from src.simple import spectral_density
-#from src.plots import compute_spectral_densities, plot_spectral_densities
-#
-#import matplotlib
-#matplotlib.rc("text", usetex=True)
-#matplotlib.rcParams["pgf.texsystem"] = "pdflatex"
-#matplotlib.rcParams["font.family"] = r"serif"
-#matplotlib.rcParams["font.serif"] = r"Palatino"
-#matplotlib.rcParams["font.size"] = 12
-#
-#np.random.seed(0)
-#
-#methods = spectral_density
-#labels = ["no non-zero check", "non-zero check"]
-#parameters = [{"m": 2000, "sigma": 0.05, "n_v": 80, "kappa": -1},
-#              {"m": 2000, "sigma": 0.05, "n_v": 80, "kappa": 1e-5}]
-#
-#A = sp.sparse.load_npz("matrices/ModES3D_1.npz")
-#colors = ["#648FFF", "#DC267F", "#FFB000"]
-#spectral_densities = compute_spectral_densities(A, methods, labels, parameters, add_baseline=False, n_t=500)
-#
-#fig, ax = plt.subplots(figsize=(6, 3))
-#plot_spectral_densities(spectral_densities, parameters, variable_parameter="kappa", ignored_parameters=["m", "sigma", "n_v", "kappa"], colors=colors, ax=ax)
-#
-#plt.savefig("thesis/plots/short_circuit_mechanism.pgf", bbox_inches="tight")
-#
-#
 import __context__
 
 import numpy as np
--- setup/generate_paper.py
+++ setup/generate_paper.py
@@ -1,16 +1,6 @@
 import __context__
 
 import os
-import glob
-
-plot_files = glob.glob("paper/plots/*.py")
-table_files = glob.glob("paper/tables/*.py")
-
-for plot_file in plot_files:
-    os.system("python " + plot_file)
-
-for table_file in table_files:
-    os.system("python " + table_file)
 
 project_name = "paper"
 os.chdir(os.path.join(os.path.abspath(""), project_name))
